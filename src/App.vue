5332ce0d474501e807318a43e76b985a5d578fc1
yusfi <yadilaksa@gmail.com>
2020-06-19 11:42:26 +0700

    add static data in App.vue and pass props to Content.vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,34 @@
 <template>
   <div id="app">
     <p>This component’s code is in {{ filename }}</p>
+    <Content :weather-data="weatherData"></Content>
   </div>
 </template>
 
 <script>
+import Content from "@/components/Content";
 export default {
   name: "app",
+  components: {
+    Content
+  },
   data() {
     return {
-      filename: "App.vue"
+      filename: "App.vue",
+      weatherData: {
+        location: "California",
+        temperature: {
+          current: "35 C"
+        },
+        highlights: {
+          uvindex: "3",
+          windstatus: {
+            speed: "20 km/h",
+            direction: "N-E"
+          },
+          visibility: "12 km"
+        }
+      }
     };
   }
 };
---
+++ src/components/Content.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <p>This child components of Content.vue are:</p>
+    <ul>
+      <li v-for="child in childComponents" :key="child.id">{{ child }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      childComponents: ["TempVarChart.vue", "Highlights.vue"]
+    };
+  },
+  methods: {},
+  computed: {}
+};
+</script>
+
+<style>
+</style>
