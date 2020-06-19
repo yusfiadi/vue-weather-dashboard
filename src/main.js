b83c76f9e734fd468a4eb3b59f4c0eb8de79ee81
yusfi <yadilaksa@gmail.com>
2020-06-19 10:46:52 +0700

    import dependencies

--- src/main.js
+++ src/main.js
@@ -1,7 +1,22 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import FusionCharts from "fusioncharts";
+import Charts from "fusioncharts/fusioncharts.charts";
+import Widgets from "fusioncharts/fusioncharts.widgets";
+import PowerCharts from "fusioncharts/fusioncharts.powercharts";
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
+import VueFusionCharts from "vue-fusioncharts";
+
+// Resolve the dependencies
+Charts(FusionCharts);
+PowerCharts(FusionCharts);
+Widgets(FusionCharts);
+FusionTheme(FusionCharts);
+
+// Globally register the components for project-wide use
+Vue.use(VueFusionCharts, FusionCharts);
 
 new Vue({
-  el: '#app',
+  el: "#app",
   render: h => h(App)
-})
+});
