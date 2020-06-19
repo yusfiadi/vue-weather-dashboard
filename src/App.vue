9e33ce838a25c66820fefb37fb37343b8cf541fc
yusfi <yadilaksa@gmail.com>
2020-06-19 17:36:09 +0700

    add data in App.vue that needed in this project

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,52 @@
         <img src="./assets/Search.svg" width="24" height="24" />
       </button>
     </div>
+    <!-- <div id="info">
+      <div class="wrapper-left">
+        <div id="current-weather">
+          {{ currentWeather.temp }}
+          <span>°C</span>
+        </div>
+        <div id="weather-desc">{{ currentWeather.summary }}</div>
+        <div class="temp-max-min">
+          <div class="max-desc">
+            <div id="max-detail">
+              <i>▲</i>
+              {{ currentWeather.todayHighLow.todayTempHigh }}
+              <span>°C</span>
+            </div>
+            <div id="max-summary">at {{ currentWeather.todayHighLow.todayTempHighTime }}</div>
+          </div>
+          <div class="min-desc">
+            <div id="min-detail">
+              <i>▼</i>
+              {{ currentWeather.todayHighLow.todayTempLow }}
+              <span>°C</span>
+            </div>
+            <div id="min-summary">at {{ currentWeather.todayHighLow.todayTempLowTime }}</div>
+          </div>
+        </div>
+      </div>
+      <div class="wrapper-right">
+        <div class="date-time-info">
+          <div id="date-desc">
+            <img src="./assets/calendar.svg" width="20" height="20" />
+            {{ currentWeather.time }}
+          </div>
+        </div>
+        <div class="location-info">
+          <div id="location-desc">
+            <img src="./assets/location.svg" width="10.83" height="15.83" style="opacity: 0.9;" />
+            {{ currentWeather.full_location }}
+            <div id="location-detail" class="mt-1">
+              Lat: {{ currentWeather.formatted_lat }}
+              <br />
+              Long: {{ currentWeather.formatted_long }}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>-->
     <DashboardContent :tempVar="tempVar" :highlights="highlights" />
   </div>
 </template>
@@ -25,23 +71,39 @@ export default {
   },
   data() {
     return {
+      weatherDetails: false,
+      location: "", // raw location from input
+      lat: "", // raw latitude from google maps api response
+      long: "", // raw longitude from google maps api response
+      completeWeatherApi: "", // weather api string with lat and long
+      rawWeatherData: "", // raw response from weather api
+      currentWeather: {
+        full_location: "", // for full address
+        formatted_lat: "", // for N/S
+        formatted_long: "", // for E/W
+        time: "",
+        temp: "",
+        todayHighLow: {
+          todayTempHigh: "",
+          todayTempHighTime: "",
+          todayTempLow: "",
+          todayTempLowTime: ""
+        },
+        summary: "",
+        possibility: ""
+      },
       tempVar: {
         tempToday: [
-          { hour: "11.00 AM", temp: "35" },
-          { hour: "12.00 PM", temp: "36" },
-          { hour: "1.00 PM", temp: "37" },
-          { hour: "2.00 PM", temp: "38" },
-          { hour: "3.00 PM", temp: "36" },
-          { hour: "4.00 PM", temp: "35" }
+          // gets added dynamically by this.getSetHourlyTempInfoToday()
         ]
       },
       highlights: {
-        uvIndex: 4,
-        visibility: 10,
+        uvIndex: "",
+        visibility: "",
         windStatus: {
-          windSpeed: "30 km/h",
-          windDirection: "30",
-          derivedWindDirection: "NNE"
+          windSpeed: "",
+          windDirection: "",
+          derivedWindDirection: ""
         }
       }
     };
