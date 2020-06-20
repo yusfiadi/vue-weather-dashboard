0916b251280946d82f5bd67999d6c95d360d5aab
yusfi <yadilaksa@gmail.com>
2020-06-20 23:15:27 +0700

    update WindStatus component

--- src/components/WindStatus.vue
+++ src/components/WindStatus.vue
@@ -6,16 +6,16 @@
         <div class="col-sm-6 col-md-6 mt-2 text-center align-middle">
           <p class="card-sub-heading mt-3">Wind Direction</p>
           <p class="mt-4">
-            <img src="../assets/winddirection.svg" height="40" width="40" />
+            <img src="../assets/Winddirection.svg" height="40" width="40" />
           </p>
-          <p class="card-value mt-4">{{ highlights.windStatus.derivedWindDirection }}</p>
+          <p class="card-value mt-4">{{ windStatus.derivedWindDirection }}</p>
         </div>
         <div class="col-sm-6 col-md-6 mt-2">
           <p class="card-sub-heading mt-3">Wind Speed</p>
           <p class="mt-4">
-            <img src="../assets/windspeed.svg" height="40" width="40" />
+            <img src="../assets/Windspeed.svg" height="40" width="40" />
           </p>
-          <p class="card-value mt-4">{{ highlights.windStatus.windSpeed }} km/h</p>
+          <p class="card-value mt-4">{{ windStatus.windSpeed }} km/h</p>
         </div>
       </div>
     </div>
