182100b5a858cec2f0a0aec0e87d0780548f3f37
yusfi <yadilaksa@gmail.com>
2020-06-20 23:17:04 +0700

    add class in Highlights component

--- src/components/Highlights.vue
+++ src/components/Highlights.vue
@@ -1,8 +1,13 @@
 <template>
-  <div id="highlights">
-    <UVIndex :uvIndex="uvIndex" />
-    <WindStatus :windStatus="windStatus" />
-    <Visibility :visibility="visibility" />
+  <div class="custom-content-card content-card card">
+    <div class="card-body pb-0">
+      <div class="content-header h4 text-center pt-2 pb-3">Highlights</div>
+      <div class="row">
+        <UVIndex :uvIndex="uvIndex" />
+        <WindStatus :windStatus="windStatus" />
+        <Visibility :visibility="visibility" />
+      </div>
+    </div>
   </div>
 </template>
 
